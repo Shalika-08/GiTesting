49a073f24f515691e4ac81cebea8c92dac470bbf
MATHIIBALAGAN <gomathi25203@gmail.com>
2025-03-11 16:25:52 +0530

    [sendbuttonset]

--- Testing/Form1.cs
+++ Testing/Form1.cs
@@ -16,5 +16,11 @@ namespace Testing
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Form2 f2 = new Form2();
+            f2.Show();
+        }
     }
 }
